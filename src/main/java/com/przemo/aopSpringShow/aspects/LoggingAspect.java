676dddf438bfd6b519117eeb8b469b6bd2bce038
przemo <p@g.com>
2020-04-27 10:09:58 +0200

    LoggingAspect works within com.przemo package

--- src/main/java/com/przemo/aopSpringShow/aspects/LoggingAspect.java
+++ src/main/java/com/przemo/aopSpringShow/aspects/LoggingAspect.java
@@ -4,12 +4,14 @@ import java.util.logging.Logger;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
+@Component
 @Aspect
 public class LoggingAspect {
     @Before("within (com.przemo..*)")
     public void logExecution() {
-        Logger.getLogger(this.getClass().getName()).info("Executing method...index");
+        Logger.getLogger(this.getClass().getName()).info("Executing method in com.przemo ...");
     }
 
 }
